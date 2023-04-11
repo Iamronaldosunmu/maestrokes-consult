0d368d983fa6703d621f54b5d391864fbc311b47
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-04-11 14:34:29 +0100

    Created the NavLink component

---
+++ src/components/Navbar/NavLink.tsx
@@ -0,0 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
+interface NavLinkProps {
+  text: string;
+  link: string;
+}
+
+const NavLink: React.FC<NavLinkProps> = ({ text, link }) => {
+  const navigate = useNavigate();
+
+  return (
+    <p onClick={() => navigate(link)} className="font-medium text-[21px] hover:cursor-pointer">
+      {text}
+    </p>
+  );
+};
+
+export default NavLink;
