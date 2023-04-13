18a52d06a107d993ee043991c5b338087e66b190
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-04-13 18:03:56 +0100

    Finished working on the navbar component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,19 +2,18 @@ import NavLink from "./NavLink";
 
 const NavBar = () => {
   return (
-    <div className="top-0 fixed left-0 right-0">
+    <div className="top-0 fixed left-0 right-0 z-20">
       <nav className="text-[#252525] text-opacity-[0.92] max-w-[1440px] px-[20px]  mx-auto pt-[26px] flex items-center justify-between">
-        <p className="text-[28px]">
+        <p className=" text-[22px] xl:text-[28px]">
           <span className="font-bold">MAESTROKES</span> CONSULT
         </p>
-        <div className="flex gap-[40px]">
+        <div className="gap-[40px] hidden lg:flex">
           <NavLink text="ABOUT US" link="/about" />
           <NavLink text="OUR WORK" link="/our-work" />
           <NavLink text="CLIENTS" link="/cLIENTS" />
           <NavLink text="CONTACT US" link="/contact-us" />
         </div>
       </nav>
-      
     </div>
   );
 };
