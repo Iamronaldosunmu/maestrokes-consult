e09e359309038d5274a59bf257db61b3d31baebc
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-04-13 18:03:14 +0100

    Removed gsap trial and scroll smoother

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,8 @@ import "./App.css";
 import NavBar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import gsap from "gsap";
-
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 function App() {
-  useLayoutEffect(() => {
-    ScrollSmoother.create({
-      smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
-    });
-  }, []);
   const [count, setCount] = useState(0);
 
   return (
