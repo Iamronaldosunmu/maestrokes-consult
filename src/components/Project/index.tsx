94e4476eaf7c470a21c9a6ddf220f8aaef3951ff
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-04-25 22:08:15 +0100

    Added the LazyLoadImage component for each image

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 interface ProjectProps {
   projectName: string;
@@ -39,7 +40,7 @@ const Project: React.FC<ProjectProps> = ({
           }}
           className="w-full h-full bg-black absolute cursor-pointer z-10"
         ></motion.div>
-        <p className="text-white absolute bottom-[65px] md:bottom-[80px] lg:bottom-[95px] left-[20px] md:left-[30px] font-bold text-[28px] md:text-[30px] lg:text-[37px] leading-[34px] lg:leading-[45px] max-w-[300px] lg:max-w-[410px] z-10">
+        <p className="text-white absolute bottom-[65px] md:bottom-[80px] lg:bottom-[95px] left-[20px] md:left-[30px] font-bold text-[28px] md:text-[30px] lg:text-[37px] leading-[34px] lg:leading-[45px] max-w-[300px] lg:max-w-[410px] z-10 cursor-pointer">
           {projectName}
         </p>
         <motion.p
@@ -47,7 +48,7 @@ const Project: React.FC<ProjectProps> = ({
             opacity: projectHovered ? 1 : 0,
             transition: { duration: 0.4 },
           }}
-          className="text-white absolute bottom-[10px] md:bottom-[20px] left-[20px] md:left-[30px] font-bold text-[20px] md:text-[25px] max-w-[420px] z-10 flex items-center"
+          className="text-white absolute bottom-[10px] md:bottom-[20px] left-[20px] md:left-[30px] font-bold text-[20px] md:text-[25px] max-w-[420px] z-10 flex items-center cursor-pointer"
         >
           VIEW PROJECT{" "}
           <motion.img
@@ -59,14 +60,21 @@ const Project: React.FC<ProjectProps> = ({
             src="/rightArrow.png"
           />
         </motion.p>
-        <motion.img
+        <motion.figure
+          className="w-full h-full"
           animate={{
             scale: projectHovered ? 1.1 : 1,
             transition: { duration: projectHovered ? 4 : 0.4 },
           }}
-          className="w-full h-full object-cover -z-10"
-          src={image}
-        />
+        >
+          <LazyLoadImage
+            width={"100%"}
+            height={"100%"}
+            effect="blur"
+            wrapperClassName="lazyLoadImageContainer"
+            src={image}
+          />
+        </motion.figure>
       </motion.div>
     </div>
   );
