1cd59d80eab8cb2f2cee36736bcd7d423df707c5
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-04-25 12:42:31 +0100

    Finished working on the mobile layout for the projects grid

---
+++ src/components/ProjectsPage/MobileLayoutAll.tsx
@@ -0,0 +1,22 @@
+import Project from "../Project";
+
+interface MobileLayoutAllProps {
+  projectData: { projectName: string; image: string }[];
+}
+
+const MobileLayoutAll: React.FC<MobileLayoutAllProps> = ({ projectData }) => {
+  return (
+    <div className="pt-[153px] w-full max-w-[1440px] mx-auto px-[20px] flex flex-col gap-[25px] mb-[20px] lg:hidden">
+      {projectData.map((project, index: number) => (
+        <Project
+          key={index}
+          height={250}
+          projectName={project.projectName}
+          image={project.image}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default MobileLayoutAll;
