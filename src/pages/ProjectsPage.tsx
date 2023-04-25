29af298dd6f9235a7de8feee093d2d8f938cc8c1
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-04-25 22:07:54 +0100

    Finished adding a link to every project

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -51,6 +51,7 @@ const ProjectsPage = () => {
       projectName: "OGUDU FORESHORE",
       image: "/Homepage/oguduforeshore.png",
       type: "RESIDENTIAL",
+      link: "/our-work/ogudu-foreshore",
     },
     {
       projectName: "PURO RESTAURANT",
@@ -62,16 +63,19 @@ const ProjectsPage = () => {
       projectName: "MAYAKI GUEST HOUSE",
       image: "/Homepage/mayakiguesthouse.png",
       type: "RESIDENTIAL",
+      link: "/our-work/mayaki-guest-house",
     },
     {
       projectName: "HOME REMODEL 1",
       image: "/Homepage/homeremodel1.jpg",
       type: "RESIDENTIAL",
+      link: "/our-work/home-remodel-1",
     },
     {
       projectName: "THE ODUYALE",
       image: "/Homepage/oduyale.jpg",
       type: "RESIDENTIAL",
+      link: "/our-work/the-oduyale",
     },
     {
       projectName: "ADMIRALTY TOWERS",
@@ -83,6 +87,7 @@ const ProjectsPage = () => {
       projectName: "BRAND CO WAREHOUSE",
       image: "/Homepage/brandcowarehouse.webp",
       type: "COMMERCIAL",
+      link: "/our-work/brand-co-warehouse",
     },
     {
       projectName: "EFOSA'S BEDROOM",
@@ -93,6 +98,12 @@ const ProjectsPage = () => {
   ]);
 
   const [filteredData, setFilteredData] = useState(projectData);
+
+  useEffect(() => {
+    setTimeout(() => window.scrollTo(0, 0), 300);
+    // window.scrollTo(0, 0);
+  }, [selectedFilter]);
+
   useEffect(() => {
     setTimeout(() => {
       setFilteredData(
@@ -106,8 +117,8 @@ const ProjectsPage = () => {
   }, [selectedFilter]);
 
   return (
-    <main>
-      <nav className="fixed top-[71px] lg:top-[48px] xl:top-[63px] bg-white w-full flex border-b border-black z-20">
+    <motion.main exit={{opacity: 0}}>
+      <motion.nav  initial={{opacity: 0}} animate={{opacity: 1, transition: {delay: 0.3, duration: 0.4}}} className="fixed top-[71px] lg:top-[48px] xl:top-[63px] bg-white w-full flex border-b border-black z-20">
         <div className="w-full max-w-[1440px] mx-auto px-[20px] flex text-[18px] xl:text-[21px] gap-[41px] pt-[12px] lg:pt-[14px] xl:pt-[16px] pb-[8px] lg:pb-[10px] xl:pb-[12px] overflow-auto no-scrollbar lg:overflow-hidden">
           <FilterItem
             text={"ALL"}
@@ -130,7 +141,7 @@ const ProjectsPage = () => {
             setSelected={setSelectedFilter}
           />
         </div>
-      </nav>
+      </motion.nav>
       <motion.div
         initial={{ opacity: 0 }}
         animate={
@@ -144,7 +155,7 @@ const ProjectsPage = () => {
         <TabletLayoutAll projectData={filteredData} />
         <MobileLayoutAll projectData={filteredData} />
       </motion.div>
-    </main>
+    </motion.main>
   );
 };
 
