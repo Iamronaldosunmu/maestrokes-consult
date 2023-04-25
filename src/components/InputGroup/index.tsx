05a38ec21f54c59647ed22cc9b4fd12d7fdd8ad3
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-04-25 22:05:36 +0100

    Finished animating the input group component with framer motion

--- src/components/InputGroup/index.tsx
+++ src/components/InputGroup/index.tsx
@@ -1,4 +1,5 @@
 import { Dispatch, SetStateAction } from "react";
+import { motion } from "framer-motion";
 
 interface InputGroupProps {
   value: string;
@@ -12,7 +13,7 @@ const InputGroup: React.FC<InputGroupProps> = ({
   placeholder,
 }) => {
   return (
-    <div className="w-full">
+    <motion.div initial={{scale: 0.95, y: 20, opacity: 0}} animate={{scale: 1, y: 0, opacity: 1, transition: {delay: 1.4}}} className="w-full">
       <input
         value={value}
         onChange={(e) => setValue(e.target.value)}
@@ -20,7 +21,7 @@ const InputGroup: React.FC<InputGroupProps> = ({
         className="text-[16px] md:text-[21px] lg:text-[24px] placeholder:text-[16px] md:placeholder:text-[21px] lg:placeholder:text-[24px] font-medium placeholder:text-black placeholder:opacity-50 pb-[10px] lg:pb-[27px] focus:outline-none w-full"
       />
       <div className="w-full bg-black h-[1px] lg:h-[2px]  bg-opacity-50 "></div>
-    </div>
+    </motion.div>
   );
 };
 
