e8514531596a38866717fd7fb8ae8c1af8faa7fd
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-04-25 12:26:10 +0100

    Removed the footer from the home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -92,24 +92,7 @@ const HomePage = () => {
           </motion.button>
         </div>
       </div>
-      <footer className="px-[20px] xl:px-[75px] pt-[50px] pb-[80px] bg-[#1C1C1C] flex flex-col lg:flex-row gap-[35px] lg:gap-0 justify-between lg:items-end">
-        <p className="text-[22px] lg:text-[27px] xl:text-[32px] text-white">
-          <span className="font-bold">MAESTROKES</span> <br /> CONSULT
-        </p>
-        <div className="text-white text-[17px] xl:text-[20px]">
-          <p>+234-(0)-8130214937</p>
-          <p>info.maestrokes@gmail.com</p>
-        </div>
-        <div className="text-white text-[17px] xl:text-[20px]">
-          <p>28 Montgomery Road, Yaba. </p>
-          <p>Lagos, Nigeria.</p>
-        </div>
-        <div className="text-white text-[17px] xl:text-[20px] flex gap-[10px] flex-col">
-          <p>Twitter</p>
-          <p>Instagram </p>
-          <p>Youtube </p>
-        </div>
-      </footer>
+
     </main>
   );
 };
