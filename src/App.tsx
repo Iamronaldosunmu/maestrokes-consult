3c13d57280aa42b0156dd8abdbb4b0554045b506
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-04-25 10:31:53 +0100

    Added the new routes and added functionality for scrolling to top on page change

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,43 @@
-import { useState, useLayoutEffect } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import NavBar from "./components/Navbar";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import ContactUsPage from "./pages/ContactUsPage";
+import Footer from "./components/Footer";
+import ProjectsPage from "./pages/ProjectsPage";
+import AboutUsPage from "./pages/AboutUsPage";
+import ProjectPage from "./pages/ProjectPage";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const location = useLocation();
+
+  useEffect(() => {
+    // setTimeout(() => window.scrollTo(0, 0), 300);
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
 
   return (
     <div className="w-full">
       <NavBar />
       <Routes>
         <Route path="/" Component={HomePage} />
+        <Route path="/contact-us" Component={ContactUsPage} />
+        <Route path="/our-work" Component={ProjectsPage} />
+        <Route path="/our-work/:projectId" Component={ProjectPage} />
+        <Route path="/about-us" Component={AboutUsPage} />
       </Routes>
+      <Footer />
     </div>
   );
 }
