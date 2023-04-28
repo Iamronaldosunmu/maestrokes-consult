32d9816f8743f62a5c209f43221fa3c44a7a70fd
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-04-28 17:27:38 +0100

    Set up the timing for changing the route

--- src/components/Navbar/NavLink.tsx
+++ src/components/Navbar/NavLink.tsx
@@ -1,15 +1,26 @@
 import { useNavigate } from "react-router-dom";
+import { Dispatch, SetStateAction } from "react";
+import { motion } from "framer-motion";
 
 interface NavLinkProps {
   text: string;
   link: string;
+  setInPageTransition: Dispatch<SetStateAction<boolean>>;
 }
 
-const NavLink: React.FC<NavLinkProps> = ({ text, link }) => {
+const NavLink: React.FC<NavLinkProps> = ({ text, link, setInPageTransition }) => {
   const navigate = useNavigate();
+  const changePage = () => {
+    setInPageTransition(true);
+    setTimeout(() => navigate(link), 2300)
+  };
 
   return (
-    <p onClick={() => navigate(link)} className="font-medium text-[18px] xl:text-[21px] hover:cursor-pointer">
+    <p
+      
+      onClick={changePage}
+      className="font-medium text-[18px] xl:text-[21px] hover:cursor-pointer"
+    >
       {text}
     </p>
   );
