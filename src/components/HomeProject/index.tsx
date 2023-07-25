34c53ba4c350b3c8121ed058a9f25d6f5ba0c405
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-07-25 14:49:49 +0100

    Fixed no color background on the home projects

--- src/components/HomeProject/index.tsx
+++ src/components/HomeProject/index.tsx
@@ -2,12 +2,14 @@ import { motion } from "framer-motion";
 import React, { useState } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
+import { useNavigate } from "react-router-dom";
 
 interface HomeProjectProps {
   text: string;
   image: string;
   showing: boolean;
   type: string;
+  link: string;
 }
 
 const HomeProject: React.FC<HomeProjectProps> = ({
@@ -15,22 +17,24 @@ const HomeProject: React.FC<HomeProjectProps> = ({
   image,
   showing,
   type,
+  link
 }) => {
   const [isZooming, setIsZooming] = useState(false);
-
+  const navigate = useNavigate();
   return (
     <>
       <motion.figure
         style={{ zIndex: showing ? 8 : 0 }}
         initial={{ opacity: 0 }}
         animate={{ opacity: showing ? 1 : 0, transition: { duration: 1.6 } }}
-        className="w-full h-full absolute -z-10 overflow-hidden"
+        className="w-full h-full absolute -z-10 overflow-hidden bg-gray-400"
       >
         <motion.div
           animate={{
             scale: isZooming ? 1.1 : 1,
             transition: { duration: 5 },
           }}
+          className=""
         >
           <LazyLoadImage  width={"100%"} height={"100vh"} effect="blur" wrapperClassName="lazyLoadImageContainer" src={image} />
         </motion.div>
@@ -43,6 +47,7 @@ const HomeProject: React.FC<HomeProjectProps> = ({
         className="absolute top-0 w-full h-full flex items-center lg:pl-[140px] px-[20px]"
       >
         <div
+          onClick={() => navigate(link)}
           onMouseEnter={() => setIsZooming(true)}
           onMouseLeave={() => setIsZooming(false)}
           className="cursor-pointer text-[32px] md:text-[40px] lg:text-[60px] font-bold max-w-[625px] text-white leading-[32px] md:leading-[40px] lg:leading-[60px]"
